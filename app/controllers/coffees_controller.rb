07dbc39eb286c9f1db8b40181eb00cd16d6d92fc
Louis Mayaud <louis.myd@gmail.com>
2018-11-27 12:52:25 +0100

    form. button validates does not trigger save

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -13,8 +13,11 @@ class CoffeesController < ApplicationController
 
   def create
     @coffee = Coffee.new(coffee_params)
-    @coffee.save
-    redirect_to coffee_path(@coffee)
+    if @coffee.save
+      redirect_to purchases_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -40,10 +43,5 @@ class CoffeesController < ApplicationController
 
   def coffee_params
     params.require(:coffee).permit(:blend_name, :provenance, :machine, :flavour, :intensity, :stock, :price_per_unit)
-    if @coffee.save
-      redirect_to coffee_path(@coffee)
-    else
-      render :new
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_164422) do
+ActiveRecord::Schema.define(version: 2018_11_27_101754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_164422) do
     t.datetime "updated_at", null: false
     t.string "tree"
     t.text "description"
+    t.string "photo"
     t.index ["user_id"], name: "index_coffees_on_user_id"
   end
 
