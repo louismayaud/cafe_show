d6434105f72d7f193cbd84c6a1b1d70ae26b4390
Louis Mayaud <louis.myd@gmail.com>
2018-11-27 10:39:09 +0100

    form

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -6,5 +6,12 @@ class CoffeesController < ApplicationController
   def create
     @coffee = Coffee.new(coffee_params)
     @coffee.save
+    redirect_to coffee_path(@coffee)
+  end
+
+  private
+
+  def coffee_params
+    params.require(:coffee).permit(:blend_name, :provenance, :machine, :flavour, :intensity, :stock, :price_per_unit)
   end
 end
