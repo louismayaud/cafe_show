3720a60a4775df69097ce09bad073ef6ae3c6dfc
Louis Mayaud <louis.myd@gmail.com>
2018-11-28 10:49:10 +0100

    photo uploader not finished

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -24,9 +24,6 @@ class CoffeesController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
     @coffee.user = current.user
 
@@ -43,10 +40,6 @@ class CoffeesController < ApplicationController
   end
 
   private
-
-  def set_coffee
-    @coffee = Coffee.find(params[:id])
-  end
 
   def coffee_params
     params.require(:coffee).permit(:blend_name, :provenance, :machine, :flavour, :intensity, :stock, :price_per_unit, :tree, :photo)
